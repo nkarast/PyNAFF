d0cef20d64d1195810d91e3679eff2b636395952
Panos Zisopoulos <pzisopou@gmail.com>
2025-02-04 14:02:39 +0100

    Update setup.py
    Signed-off-by: Panos Zisopoulos <pzisopou@gmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = 'PyNAFF',
-        version = '1.1.6',
+        version = '1.1.9',
         license='GPLv3',
         description = 'A Python module that implements the Numerical Analysis of Fundamental Frequencies (NAFF) algorithm', 
         author='Nikos Karastathis',
